a6e354e639db53587cfdafc0cd0da315f9f4b679
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-12-24 12:28:12 +0200

    Fixed up code style

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // This is a simple module for Node.js made with the included Math functions of Node.js. Usage is simple and an example has been included
 module.exports = (min, max, nums) => {
-  max = max + 1;
-  var arr1 = [];
-  if (nums === undefined) {
-    var loopfor = 1;
-  } else {
-    var loopfor = nums;
-  }
-  for (i = 0; i < loopfor; i++) {
-    var randomnumtmp = Math.floor((Math.random() * (max - min)) + min);
-      arr1.push(randomnumtmp);
-  }
-  return arr1;
+	max += 1;
+	var arr1 = [];
+	var loopfor = 0;
+	if (nums === undefined) {
+		loopfor = 1;
+	} else {
+		loopfor = nums;
+	}
+	for (var i = 0; i < loopfor; i++) {
+		var randomnumtmp = Math.floor((Math.random() * (max - min)) + min);
+		arr1.push(randomnumtmp);
+	}
+	return arr1;
 };
